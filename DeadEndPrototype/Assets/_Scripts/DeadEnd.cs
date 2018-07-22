5edbd2286788549087aa0c6927a319b6c812520f
Bo yo <keepintine@gmail.com>
2018-07-22 11:00:29 +0300

    Init project

---
+++ DeadEndPrototype/Assets/_Scripts/DeadEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadEnd : MonoBehaviour {
+
+    public static DeadEnd S;
+    public static bool DEBUG = true;
+
+    private void Awake() {
+        S = this;
+    }
+
+    public void HeroCollWithGround() {
+        if (DEBUG) print("HeroCollWithGround");
+    }
+}
---
+++ DeadEndPrototype/Assets/_Scripts/Hero.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+    public static Hero S;
+
+    public bool ________________;
+
+    public float height;
+    public bool onGround = false;
+
+    private void Awake() {
+        S = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    void Move() {
+        Vector3 pos = transform.position;
+        if (onGround) {
+            pos.y = height;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        print("Collision with mag");
+        if (collision.gameObject.tag == "Ground") {
+            // Если герой взаимодействовал с землёй
+            // Значит персонаж уже на земле
+            onGround = true;
+            DeadEnd.S.HeroCollWithGround();
+            height = collision.gameObject.transform.position.y;
+        }
+    }
+}
