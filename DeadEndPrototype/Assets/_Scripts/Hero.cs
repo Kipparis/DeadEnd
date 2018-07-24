0968176b9bc060036b8b19183906f5624860b7e2
Un <keepintine@gmail.com>
2018-07-24 21:13:11 +0300

    Add rotation of player

--- DeadEndPrototype/Assets/_Scripts/Hero.cs
+++ DeadEndPrototype/Assets/_Scripts/Hero.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Hero : MonoBehaviour {
 
     public static Hero S;
+    public static float HERO_HEIGHT = 4f;
+
+    public float speed = 1f;
+    public float rotEasing = 0.99f;
+
+    public Vector3 tPos;
 
     public bool ________________;
 
     public float height;
     public bool onGround = false;
 
-    Transform characterTrans;
+    public Sword[] swords;
 
     private void Awake() {
-        S = this;
-
-        characterTrans = transform.Find("CharacterTransform");
+        S = this; 
+        
+        
     }
 
     // Use this for initialization
@@ -26,21 +32,53 @@ public class Hero : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Vector3 pos = characterTrans.position;
-        pos.y = height + 1;
-        characterTrans.position = pos;
-        transform.position = pos;
+        Attack();
+
+        Move();
+    }
+
+    void Attack() {
+        if (Input.GetMouseButtonUp(0)) {
+            print("Hero attack");
+            // Игрок нажал левую кнопку мыши
+            // Значит мы бьём мечом ( который мы можем лутать или сменять
+            
+        }
+    }
+
+    void Move() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        tPos = transform.position;
+        if (height != 0) tPos.y = height + Hero.HERO_HEIGHT / 2f;
+        tPos.x += h * speed * Time.deltaTime;
+        tPos.z += v * speed * Time.deltaTime;
+
+        Vector3 direction = tPos - transform.position;
+
+        transform.position = tPos;
+
+        Vector3 NextDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 
+            Input.GetAxisRaw("Vertical"));
+        //if (NextDir != Vector3.zero)
+        //    transform.rotation = Quaternion.Lerp(Quaternion.LookRotation(NextDir),
+        //        transform.rotation, rotEasing);
+        //transform.rotation = Quaternion.LookRotation(fPos, Vector3.up * 3);
+        if (direction.magnitude != 0) {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(direction), 0.1f);
+        }
     }
 
     private void FixedUpdate() {
 
     }
 
-    private void OnTriggerEnter(Collider other) {
-        print("Trigger hero");
-        if (other.tag == "Ground") {
-            // Это земля
-            height = other.gameObject.transform.position.y;
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Ground") {
+            onGround = true;
+            height = collision.gameObject.transform.position.y;
         }
     }
 }
---
+++ DeadEndPrototype/Assets/_Scripts/Sword.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+    public string name;
+
+    // У каждого эффекта будет пометка - влияет на персонажа или на врага 
+    // или на кого вообще
+    public List<string> specials;   // Пока что строки
+}
